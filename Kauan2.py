f07b750d63e153b5b986c8cd4e60d9ccfb77edb1
Kauan Stipp <kauan.stipp.pedroso@escola.pr.gov.br>
2026-02-19 09:45:06 -0300

    Teste1

--- Kauan2.py
+++ Kauan2.py
@@ -1,1 +1,6 @@
 print("Seja bem vindo ")
+
+print("Corinthians surgido em 1910 é uma grande potência do futebol brasileiro sendo um dos grandes paulista colecionando títulos ao longo dos anos.")
+
+git config --global user.email "kauan.stipp.pedroso@escola.pr.gov.br"
+git config --global user.name "Kauan Stipp"
