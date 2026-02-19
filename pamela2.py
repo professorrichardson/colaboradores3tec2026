18fa209d8a8fe83df3cf1e7de88df101e048c991
pamel44 <batista.paula.pamela@escola.pr.gov.br>
2026-02-19 09:45:46 -0300

    teste

--- pamela2.py
+++ pamela2.py
@@ -4,3 +4,9 @@ def nome (props):
     print(props)
 
 nome(nome)    
+
+print("seja bem vindo")
+
+git config --global user.email "batista.paula.pamela@escola.pr.gov.br"
+git config --global user.name "pamel44"
+
